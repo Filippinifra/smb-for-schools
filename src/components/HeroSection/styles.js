54745084d126f64ce9c7c2a18ad106d9f77107be
filippinifra <filippinifra123@gmail.com>
2020-11-03 00:22:36 +0100

    Handle different size of hero image background

--- src/components/HeroSection/styles.js
+++ src/components/HeroSection/styles.js
@@ -1,15 +1,25 @@
 import { SECOND_COLOR } from "constants/palette";
 import styled from "styled-components";
 
-import heroImage from "images/heroImage.png";
+import heroImageDesktop from "images/heroImages/desktop.png";
+import heroImageMobile from "images/heroImages/mobile.png";
+import heroImageTablet from "images/heroImages/tablet.png";
 
 export const BackgroundWrapper = styled.div`
   position: fixed;
   background-position: center center;
   background-size: cover;
   background-repeat: no-repeat;
-  background-image: url(${heroImage});
+  background-image: url(${heroImageDesktop});
   width: 100vw;
   height: 100vh;
   background-color: ${SECOND_COLOR};
+
+  @media only screen and (max-width: 1100px) {
+    background-image: url(${heroImageTablet});
+  }
+
+  @media only screen and (max-width: 650px) {
+    background-image: url(${heroImageMobile});
+  }
 `;
