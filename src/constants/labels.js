424e86a9bf0da9214aea0cce964a02a850b13497
filippinifra <filippinifra123@gmail.com>
2020-11-08 13:01:11 +0100

    Fix labels

--- src/constants/labels.js
+++ src/constants/labels.js
@@ -1,14 +1,14 @@
 export const LABELS = {
   whatWeDo:
-    "Siamo un brand di abbigliamento streetwear nato a Verona nel 2016. La nostra specialità? Creare capi personalizzati e alla moda. Oltre ad avere uno store online nel quale proponiamo le nostre collezioni da anni collaboriamo con gli istitui di secondo grado per realizzare il loro vestiario d'istituto.",
+    "Siamo un brand di abbigliamento streetwear nato a Verona nel 2015. La nostra forza? Creare capi personalizzati e alla moda. Oltre ad avere uno store online, nel quale proponiamo le nostre collezioni, da anni collaboriamo con gli istituti di secondo grado per realizzare il loro vestiario d'istituto.",
   graphics:
-    "Tutto ciò che viene stampato sui nostri capi regala un tocco magico al prodotto finale. Ogni grafica è realizzata dalle mani creative e sapienti dei nostri designer. Le grafiche sono indirizzate dalle richieste dell'istituto e customizzate per regalare una smufatura streetwear.",
+    "Tutto ciò che viene stampato sui nostri capi regala un tocco unico al prodotto finale. Ogni grafica è realizzata dalle mani creative e sapienti dei nostri designer. Non avete un logo o ne vorreste uno personalizzato? Siamo capaci di realizzare ogni idea ottenendo un capo capace di adattarsi non solo al contesto scolastico. Il servizio di creazione del logo è gratuito e vi permetterà di valorizzare l'identità del vostro istituto.",
   products:
-    "Siamo in grado di fare qualsiasi cosa e di qualsiasi colore. Le richieste particolari ci piacciono perchè ci mettono alla prova e siamo fieri di portarle a termine.",
+    "Mettiamo a disposizione un'ampia scelta di prodotti e colori. L'offerta varia dalle classiche t-shirt e felpe fino a capi e accessori più ricercati e particolari come polo, cappellini da baseball o invernali, pantaloncini sportivi, maglie a maniche lunghe, giacche e zaini. Le richieste particolari ci piacciono perchè ci mettono alla prova e siamo fieri di portarle a termine.",
   print1:
-    "La stampa che proponiamo è una stampa di qualità. Nel tempo l'usura è minima e il colore non sbiadisce. È possibile stampare ovunque: sulle maniche di una maglietta a maniche lunghe o sul cappuccio di una felpa.",
+    "La serigrafia é un processo di stampa a uno o più colori che consiste nel trasferire l’inchiostro sfruttando un movimento meccanico a pressione. Il risultato di questa tecnica é una stampa dai colori vivi e un capo resistente ai lavaggi.",
   print2:
-    "Spesso viene scelta la stampa a caldo al posto del ricamo. Il ricamo tuttavia è ricercato e sempre di moda, è piacevole al tatto e crea un piccolo rilievo sul capo che lo rende unico.",
+    "Il ricamo é una tecnica di personalizzazione ad alta qualità e resistenza ed è il miglior modo per aggiungere valore ai vostri capi di abbigliamento.",
   about:
-    "Siamo dinamici. Siamo creativi. Siamo simpatici. Siamo aperti a qualsiasi forma di collaborazione. Per qualsiasi domanda o informativa siamo disponibili a essere contattai. Saint Mary Boulevard, we brand where we live!",
+    "Saint Mary Boulevard nasce nel 2015 dall’idea di due fratelli veronesi. Il loro concept: identificare una compagnia di amici sotto un nome ed un logo comune nei quali i ragazzi del quartiere potessero riconoscersi. Il nome deriva dal luogo dove tutto è nato, Santa Maria di Negrar (VR), inglesizzato ‘‘Saint Mary’’. Successivamente è stato aggiunto ‘‘Boulevard’’ per dare una connotazione giovanile. Il logo rappresenta una mano che forma una ‘‘S’’ maiuscola, iniziale di Saint, con la quale i ragazzi usano salutarsi. Le T-Shirt furono inizialmente prodotte solo per i ragazzi della zona che, indossandole, fecero scaturire l’interesse dei coetanei di Verona e provincia. Saint Mary Boulevard, We brand where we live",
 };
